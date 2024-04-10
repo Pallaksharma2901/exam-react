d327c3ba641f0673a6d639a2808772c343c3cb1f
pallaksharma2901 <pallakmanojsharma@gmail.com>
2024-04-10 23:40:03 +0530

    firt commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Home = () => {
+	return (
+		<div className="bg-green-100 h-dvh">
+			<section className="text-gray-900 body-font   bg-green-100">
+				<div className="banner">
+					<h1 className="absolute text-7xl text-[#ad512f]">Truly Immenese Relax Place</h1>
+					<button className="absolute">View More</button>
+				</div>
+			</section>
+		</div>
+	)
+}
+
+export default Home
